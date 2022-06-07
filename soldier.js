6c2a8df407851f8f93cb3bfd428f77c1d413fc1d
Malsagov <rus.malsagov177@mail.ru>
2022-06-07 12:19:32 +0300

    Доработал функционал солдата, что бы он мог воевать

--- soldier.js
+++ soldier.js
@@ -6,7 +6,39 @@ const soldier = {
     ammunition: 30,
   },
   supplies: 3,
-  fire: function () {},
-  reload: function () {},
-  hurt: function () {},
+  fire: function () {
+    const ammunitionTrue = "бах-бах";
+    const ammunitionFalse = "обойма пуста. Перезарядитесь.";
+    if (this.weapon.ammunition > 0) {
+      console.log(ammunitionTrue);
+      this.weapon.ammunition--;
+      return false;
+    } else {
+      console.log(ammunitionFalse);
+      return false;
+    }
+  },
+  reload: function () {
+    const reloadAmmunitTrue = "перезарядка ...";
+    const reloadAmmunitFalse = "не осталось припасов";
+    if (this.supplies !== 0) {
+      this.ammunition = 30;
+      this.supplies--;
+      console.log(reloadAmmunitTrue);
+      return false;
+    } else {
+      console.log(reloadAmmunitFalse);
+      return false;
+    }
+  },
+  hurt: function () {
+    this.health--;
+    if (this.health === 0) {
+      console.log("Ты проиграл");
+    }
+  },
 };
+
+// soldier.reload();
+soldier.hurt();
+console.log(soldier);
