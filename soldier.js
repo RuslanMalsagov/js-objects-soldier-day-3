bde4cbecd1d0c4189b3fbf9fd972e6f5cb815b74
Malsagov <rus.malsagov177@mail.ru>
2022-06-07 11:36:23 +0300

    Создал персонажа игры

---
+++ soldier.js
@@ -0,0 +1,12 @@
+const soldier = {
+  name: "Bob",
+  health: 10,
+  weapon: {
+    nameWeapon: "М416",
+    ammunition: 30,
+  },
+  supplies: 3,
+  fire: function () {},
+  reload: function () {},
+  hurt: function () {},
+};
